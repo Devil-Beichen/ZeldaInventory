b347ba5d2078681a4df928c85cf884223a17afd0
陈华 <2394439184@qq.com>
2025-02-03 18:09:17 +0800

    钱的结构体与数据表格添加

---
+++ Content/Script/Inventory/Blueprints/BP_ZeldaInventory_GameInstance.lua
@@ -0,0 +1,13 @@
+---@diagnostic disable: missing-parameter
+--
+-- DESCRIPTION
+--
+-- @COMPANY **
+-- @AUTHOR **
+-- @DATE ${date} ${time}
+--
+
+---@type BP_ZeldaInventory_GameInstance_C
+local M = UnLua.Class()
+
+return M
